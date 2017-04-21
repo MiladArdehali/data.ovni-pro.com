50e32a531d8cbd71c804ce9e029f84eb7fea9cb5
milad ARDEHALI <milad.ardehali@gmail.com>
2017-04-21 22:25:55 +0200

    mise à jour du nom de la base de donnée

--- ConfirmForm.php
+++ ConfirmForm.php
@@ -9,7 +9,7 @@ function envoyerValeur(){
 		$loginBDD = $_SESSION['loginBDD'];
 		$password = $_SESSION['password'];
 		$database = $_SESSION['database'];
-		$connexion = new PDO("mysql:host=$serveur;dbname=data",$loginBDD,$password);
+		$connexion = new PDO("mysql:host=$serveur;dbname=$database",$loginBDD,$password);
 		$connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$statement = $connexion -> prepare("INSERT INTO suivi (id, date, heure, nomAnnexe, commentaire, projet, technologie) VALUES (null, ?,?, ?, ?, ?, ?)");
 		$statement->bindParam(1, $_POST['date']);
